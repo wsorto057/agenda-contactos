e68707a5fcd973bc1088a71d644177678f805fdd
William_Sorto <william.sorto.alv@gmail.com>
2021-12-19 21:19:40 -0600

    primer commit

---
+++ agenda-contactos/src/main/java/com/agenda/contactos/AgendaContactosApplication.java
@@ -0,0 +1,13 @@
+package com.agenda.contactos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaContactosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaContactosApplication.class, args);
+	}
+
+}
---
+++ agenda-contactos/src/test/java/com/agenda/contactos/AgendaContactosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenda.contactos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaContactosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
